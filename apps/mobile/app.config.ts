d4b13eaa975185432fc3e2e5c97013bd94ec4a44
Nestor Lesna <115150372+nestorlesna@users.noreply.github.com>
2026-04-22 20:00:48 -0300

    chore: bump version to 1.0.10

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'RunningLes',
   slug: 'running-les',
-  version: '1.0.8',
+  version: '1.0.10',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'dark',
